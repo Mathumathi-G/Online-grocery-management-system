672df496a93a44082c6b0282c1853ca24a280566
vignesh <vigneshphr2002@gmail.com>
2025-02-06 13:47:05 +0530

    ggggggggg

---
+++ frontend/src/Pages/About/About.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header/Header";
+import "./About.css";
+// import MetaData from "../MetaData";
+
+const About = () => {
+  document.title = "About Us";
+  return (
+    <>
+      <Header />
+
+      <div className="about-section-container">
+        <h1 className="Heading">
+          About <span>Us</span>
+        </h1>
+        {/* <MetaData title={'About Us'} /> */}
+        <div className="about-section-box">
+        
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default About;
